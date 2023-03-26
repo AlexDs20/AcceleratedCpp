aa598910db179294b42573441211028a29c1149a
Alexandre De Spiegeleer <alex-ds@live.be>
2023-03-26 23:20:10 +0200

    Add "inline" for grade function

--- projects/4.2-Organising-programs-several-students/include/grade.hpp
+++ projects/4.2-Organising-programs-several-students/include/grade.hpp
@@ -4,7 +4,9 @@
 #include <vector>
 #include "student_info.hpp"
 
-double grade(double midterm, double final, double homework);
+// inline: -> copy the body of the function where the function is called
+// => avoid having to call the function
+inline double grade(double midterm, double final, double homework);
 double grade(double midterm, double final, const std::vector<double>& homework);
 double grade(const Student_info&);
 
