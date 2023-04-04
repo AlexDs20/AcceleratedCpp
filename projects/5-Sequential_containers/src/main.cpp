53fb6ca4e4036c711c042a30913bbd0d3f873b90
Alexandre De Spiegeleer <alex-ds@live.be>
2023-04-04 13:20:22 +0200

    Add string operations

--- projects/5-Sequential_containers/src/main.cpp
+++ projects/5-Sequential_containers/src/main.cpp
@@ -13,7 +13,10 @@
 
 
 std::vector<std::string> split_string_by_spaces(const std::string&);
-
+std::vector<std::string> frame(const std::vector<std::string>&);
+std::vector<std::string>::size_type width(const std::vector<std::string>& v);
+std::vector<std::string> vconcat(const std::vector<std::string>& top, const std::vector<std::string>& bottom);
+std::vector<std::string> hconcat(const std::vector<std::string>& left, const std::vector<std::string>& right);
 
 int main()
 {
@@ -25,6 +28,23 @@ int main()
     std::vector<std::string> words = split_string_by_spaces(text);
     for (std::vector<std::string>::const_iterator iter = words.begin(); iter != words.end(); ++iter)
         std::cout << *iter << std::endl;
+
+    // Example of framing
+    std::vector<std::string> p = {
+        "This is an",
+        "example",
+        "to",
+        "illustrate",
+        "framing",
+    };
+
+    std::vector<std::string> new_p = frame(p);
+    p = vconcat(p, new_p);
+    p = hconcat(p, frame(p));
+
+    for (std::vector<std::string>::size_type i = 0; i != p.size(); ++i){
+        std::cout << p[i] << std::endl;
+    }
 
     if (false) {
         while(read(std::cin, record)){
@@ -70,6 +90,65 @@ int main()
     }
 
     return 0;
+}
+
+std::vector<std::string> vconcat(const std::vector<std::string>& top, const std::vector<std::string>& bottom){
+    std::vector<std::string> ret = top;
+
+    // naive way of doing it
+    for (std::vector<std::string>::const_iterator it = bottom.begin(); it!=bottom.end(); ++it)
+        ret.push_back(*it);
+
+    // alternative, use built-in insert
+    ret.insert(ret.end(), bottom.begin(), bottom.end());
+
+    return ret;
+}
+
+std::vector<std::string> hconcat(const std::vector<std::string>& left, const std::vector<std::string>& right){
+    std::vector<std::string> ret;
+    std::string::size_type w_left = width(left) + 1;
+    std::string::size_type w_right = width(right);
+
+    std::vector<std::string>::size_type maxrow = std::max(left.size(), right.size());
+
+    for (std::vector<std::string>::size_type i = 0; i!= maxrow; ++i){
+        std::string append(w_right, ' '), prepend(w_left, ' ');
+
+        if (i < left.size())
+            prepend = left[i] + std::string(w_left - left[i].size(), ' ');
+
+        if (i < right.size())
+            append = right[i] + std::string(w_right - right[i].size(), ' ');
+
+        ret.push_back(prepend + append);
+    }
+    return ret;
+}
+
+std::vector<std::string>::size_type width(const std::vector<std::string>& v){
+    std::vector<std::string>::size_type w = 0;
+    std::vector<std::string>::size_type i;
+    for (i = 0; i!= v.size(); ++i)
+        w = std::max(w, v[i].size());
+    return w;
+}
+
+std::vector<std::string> frame(const std::vector<std::string>& v) {
+    typedef std::vector<std::string> vec_string;
+    vec_string ret;
+    vec_string::size_type w = width(v);
+    std::string border(w + 4 , '*');
+
+    ret.push_back(border);
+
+    for(vec_string::size_type i = 0; i!= v.size(); ++i){
+        ret.push_back("* " + v[i] + std::string(w - v[i].size(), ' ') + " *");
+    }
+
+    ret.push_back(border);
+
+    return ret;
 }
 
 std::vector<std::string> split_string_by_spaces(const std::string& s)
