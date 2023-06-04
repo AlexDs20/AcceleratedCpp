ca4a628014f1f32ef14dbc843b074f2334ab99d8
Alexandre De Spiegeleer <alex-ds@live.be>
2023-06-04 22:44:59 +0200

    Add input to main char** argv

--- projects/10-Managing-memory/src/main.cpp
+++ projects/10-Managing-memory/src/main.cpp
@@ -4,7 +4,7 @@
 #include "string_literals.hpp"
 #include "arrays_of_pointers.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
     // array_and_pointers();
     // array();
@@ -14,6 +14,14 @@ int main()
     // pointer_arithmetic();
     // indexing_10_1_5();
     // string_literals();
-    arrays_of_pointers();
+    // arrays_of_pointers();
+
+    // *argv* is a pointer to the initial element of an array of pointer
+    std::cout << argc << std::endl;
+    if (argc>1){
+        for (int i=0; i<argc; ++i)
+            std::cout << *argv[i] << " ";        // argv[i] is a char*
+        std::cout << std::endl;
+    }
     return 0;
 }
