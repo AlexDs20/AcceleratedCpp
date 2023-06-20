6d23194edf216aa92c360b6cc3a0111ea5c7809f
Alexandre De Spiegeleer <alex-ds@live.be>
2023-06-20 21:22:50 +0200

    init chap11

---
+++ projects/11-Defining-abstract-data-types/premake5.lua
@@ -0,0 +1,14 @@
+project "11-Defining-abstract-data-types"
+    kind "ConsoleApp"
+    language "C++"
+
+    includedirs
+    {
+        "src/"
+    }
+
+    files
+    {
+        "src/**.cpp",
+        "src/**.hpp"
+    }
