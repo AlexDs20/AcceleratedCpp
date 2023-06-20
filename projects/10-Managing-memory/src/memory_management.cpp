eb0dcf57593070816c610a032d28d9d1b1dd7bb2
Alexandre De Spiegeleer <alex-ds@live.be>
2023-06-20 19:57:28 +0200

    Add dynamically allocated array + example

--- projects/10-Managing-memory/src/memory_management.cpp
+++ projects/10-Managing-memory/src/memory_management.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstring>          // strlen
+#include <algorithm>        // copy
 #include "memory_management.hpp"
 
 
@@ -13,12 +16,48 @@ int* static_valid_pointer(){
     return &x;
 };
 
+
 int* dynamic_valid_pointer(int value){
     // allocates an int object
     // object stays until program ends or run delete pointer
     int* pt = new int(value);
     return pt;
     // alternatively: return new int(value);
+};
+
+
+char* duplicate_chars(const char* p){
+    std::size_t length = strlen(p) + 1;
+    char* result = new char[length];
+    std::copy(p, p+length, result);
+    return result;
+}
+
+
+void dynamically_allocated_array(){
+    int l = 10;
+    int* p = new int[l];        // default initialized
+
+    std::cout << "Dynamically allocated array: " << std::endl;
+    for (int i = 0; i != l; ++i)
+        std::cout << p[i] << ", ";
+    std::cout << std::endl;
+
+    std::cout << "Dynamically allocated converted to vector: " << std::endl;
+    std::vector<int> my_vec(p, p+l);
+    for (std::vector<int>::const_iterator it=my_vec.begin(); it!=my_vec.end(); ++it)
+        std::cout << *it << ", ";
+    std::cout << std::endl;
+
+    delete[] p;
+
+    std::cout << std::string(20, '-') << std::endl;
+    std::cout << "Dynamically allocated example: " << std::endl;
+    char* text = duplicate_chars("This is my char that I duplicate");
+    for (size_t i=0; i!=strlen(text)+1; ++i)
+        std::cout << text[i];
+    std::cout << std::endl;
+    delete[] text;
 };
 
 
@@ -33,6 +72,7 @@ void dynamically_allocated(){
     std::cout << "Value at: " << pt2 << " is: " << *pt2 << std::endl;
 
     // Dynamically allocating requires that we clean ourselves the pointer
+    // It frees the space
     delete pt1, pt2;
 };
 
@@ -71,4 +111,9 @@ void three_kinds_of_mem_management(){
     std::cout << "Dynamically allocated: " << std::endl;
     std::cout << std::string(50, '-') << std::endl;
     dynamically_allocated();
+
+    std::cout << "Dynamically allocated array: " << std::endl;
+    std::cout << std::string(50, '-') << std::endl;
+    dynamically_allocated_array();
+
 }
